5d08974a19d93a073b50ee4a74c2ab49ffd665b1
Näpflin Simon Tobias (naep) <naep@zhaw.ch>
2018-08-30 18:08:12 +0200

    added json entries

--- src/components/measures/mock-measures.ts
+++ src/components/measures/mock-measures.ts
@@ -1,86 +1,15 @@
 import { Measure } from './measure';
-/* Essential Eight
-export const MEASURES: Measure[] = [
-    {
-        "name": "Application whitelisting",
-        "description": "Application whitelisting of approved/trusted programs to prevent execution of unapproved/malicious programs including .exe, DLL, scripts (e.g. Windows Script Host, PowerShell and HTA) and installers.",
-        "isMalware": true,
-        "isCompromise": false,
-        "isSocialEngineering": false,
-        "isMisuse": false,
-        "isTheft": false
-      },
-      {
-        "name": "Patch applications",
-        "description": "Patch applications e.g. Flash, web browsers, Microsoft Office, Java and PDF viewers. Patch/mitigate computers with ‘extreme risk’ vulnerabilities within 48 hours. Use the latest version of applications.",
-        "isMalware": true,
-        "isCompromise": true,
-        "isSocialEngineering": false,
-        "isMisuse": false,
-        "isTheft": false
-      },
-      {
-        "name": "Configure Microsoft Office macro settings",
-        "description": "Configure Microsoft Office macro settings to block macros from the Internet, and only allow vetted macros either in ‘trusted locations’ with limited write access or digitally signed with a trusted certificate.",
-        "isMalware": true,
-        "isCompromise": false,
-        "isSocialEngineering": false,
-        "isMisuse": false,
-        "isTheft": false
-      },
-      {
-        "name": "User application hardening",
-        "description": "Configure web browsers to block Flash (ideally uninstall it), ads and Java on the Internet. Disable unneeded features in Microsoft Office (e.g. OLE), web browsers and PDF viewers.",
-        "isMalware": true,
-        "isCompromise": false,
-        "isSocialEngineering": false,
-        "isMisuse": false,
-        "isTheft": false
-      },
-      {
-        "name": "Restrict administrative privileges",
-        "description": "Restrict administrative privileges to operating systems and applications based on user duties. Regularly revalidate the need for privileges. Don't use privileged accounts for reading email and web browsing.",
-        "isMalware": false,
-        "isCompromise": false,
-        "isSocialEngineering": false,
-        "isMisuse": true,
-        "isTheft": false
-      },
-      {
-        "name": "Patch operating systems",
-        "description": "Patch/mitigate computers (including network devices) with ‘extreme risk’ vulnerabilities within 48 hours. Use the latest operating system version. Don't use unsupported versions.",
-        "isMalware": true,
-        "isCompromise": true,
-        "isSocialEngineering": false,
-        "isMisuse": false,
-        "isTheft": false
-      },
-      {
-        "name": "Multi-factor authentication",
-        "description": "Multi-factor authentication including for VPNs, RDP, SSH and other remote access, and for all users when they perform a privileged action or access an important (sensitive/high-availability) data repository.",
-        "isMalware": false,
-        "isCompromise": true,
-        "isSocialEngineering": true,
-        "isMisuse": true,
-        "isTheft": false
-      },
-      {
-        "name": "Daily backups",
-        "description": "Daily backups of important new/changed data, software and configuration settings, stored disconnected, retained for at least three months. Test restoration initially, annually and when IT infrastructure changes.",
-        "isMalware": false,
-        "isCompromise": false,
-        "isSocialEngineering": false,
-        "isMisuse": false,
-        "isTheft": false
-      }
-];
-*/
 
 // CyberComp Basics
 export const MEASURES: Measure[] = [
   {
-    "name": "Restrict administrative privileges",
-    "description": "",
+    "name": "Automatisches/Regelmässiges Update und Patches für Firmware, Betriebssystem und Anwendungen",
+    "description": "Patch applications e.g. Flash, web browsers, Microsoft Office, Java and PDF viewers. Patch/mitigate computers with extreme risk vulnerabilities within 48 hours. Use the latest version of applications. Why: Security vulnerabilities in applications can be used to execute malicious code on systems. Patch operating systems. Patch/mitigate computers (including network devices) with ‘extreme risk’ vulnerabilities within 48 hours. Use the latest operating system version. Don't use unsupported versions. Why: Security vulnerabilities in operating systems can be used to further the compromise of systems.",
     "modeOfAttack":["Malware", "Social Engineering", "Misuse"]
+  },
+  {
+    "name": "Regelmässige Datensicherung",
+    "description": "Daily backups of important new/changed data, software and configuration settings, stored disconnected, retained for at least three months. Test restoration initially, annually and when IT infrastructure changes. Why: To ensure information can be accessed again following a cyber security incident (e.g. after a successful ransomware incident).",
+    "modeOfAttack":["Malware", "Compromise", "Social Engineering", "Misuse", "Theft"]
   }
 ];
